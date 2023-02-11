3b508a52dd7d51ed633eae954ab65ec8521c425b
DreStation <drestation1@gmail.com>
2023-02-11 12:15:34 -0500

    added another solution

--- basics/palindrome.py
+++ basics/palindrome.py
@@ -32,3 +32,10 @@ while True:
         print(str + " is a palindrome")
     else:
         print(str + " is not a palindrome")
+
+
+# Alternative way
+def is_palindrome(str):
+    if str == str[::-1]:
+        return True
+    return False
